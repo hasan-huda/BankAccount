fcf670fc70df665f964297b057a33158056d4151
Hasan Huda <hasanhuda037@gmail.com>
2023-02-04 14:14:28 -0500

    Completed class

--- BankAccount.java
+++ BankAccount.java
@@ -2,7 +2,7 @@ public class BankAccount{
   private double checkingBalance;
   private double savingsBalance;
   private static int numberOfAccounts = 0;
-  private static double totalAmount=0;
+  private static double totalAmountAll=0;
   
   public BankAccount(){
     checkingBalance=0;
@@ -22,4 +22,40 @@ public class BankAccount{
   public double getSavingsBalance(){
     return savingsBalance;
   }
+
+  public void deposit(String accountType, double amount){
+    if(accountType=="Checking" || accountType== "checking"){
+      checkingBalance+=amount;
+    } else {
+      savingsBalance+=amount;
+    }
+    totalAmountAll+=amount;
+  }
+
+  public void withdraw(String accountType, double amount){
+    if(accountType=="Checking" || accountType== "checking"){
+      if(checkingBalance>=amount){
+        checkingBalance-=amount;
+        totalAmountAll-=amount;
+        System.out.println("Successful Withdrawal from checkings");
+      } else {
+        System.out.println("Insufficient Funds in checkings");
+      }
+    } else {
+      if(savingsBalance>=amount){
+        savingsBalance-=amount;
+        totalAmountAll-=amount;
+        System.out.println("Successful Withdrawal from savings");
+      } else {
+        System.out.println("Insufficient Funds in savigns");
+      }
+    }
+  }
+
+  public void displayBalances(){
+    System.out.println("Checkings Balance: "+checkingBalance);
+    System.out.println("Savings Balance: "+savingsBalance);
+    System.out.println("Total Balance: "+(savingsBalance+checkingBalance));
+  }
+
 }
